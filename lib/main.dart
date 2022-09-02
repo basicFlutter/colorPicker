72493bdbe0ab56d78c587748c6114e14ba8faaea
basicFlutter <farshid3745@gmail.com>
2022-09-02 22:54:33 +0430

    color picker in flutter

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:color_picker/color_picker.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const ColorPicker()
+    );
+  }
+}
